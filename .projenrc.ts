2054c581263f6a4db2b1f95d9578a6f48e2c8a95
Kaizen Conroy <zen.conroy@gmail.com>
2023-11-27 15:21:58 -0500

    bump in projenrc

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,13 +1,14 @@
-import { awscdk } from "projen";
+import { awscdk } from 'projen';
+
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: "AWS",
-  authorAddress: "aws-cdk-dev@amazon.com",
-  cdkVersion: "2.1.0",
-  defaultReleaseBranch: "main",
-  name: "awscdk-lambda-dotnet",
+  author: 'AWS',
+  authorAddress: 'aws-cdk-dev@amazon.com',
+  cdkVersion: '2.80.0',
+  defaultReleaseBranch: 'main',
+  name: 'awscdk-lambda-dotnet',
   projenrcTs: true,
   release: false,
-  repositoryUrl: "https://github.com/cdklabs/awscdk-lambda-dotnet.git",
+  repositoryUrl: 'https://github.com/cdklabs/awscdk-lambda-dotnet.git',
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="awscdk-lambda-dotnet.Hello"></a>
+
+#### Initializers <a name="Initializers" id="awscdk-lambda-dotnet.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'awscdk-lambda-dotnet'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#awscdk-lambda-dotnet.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="awscdk-lambda-dotnet.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
